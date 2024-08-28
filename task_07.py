22a6f0673f071f3e1ea9c83af0d931846d65579b
Nio <nio@example.com>
2024-08-28 03:41:01 +0300

    7

---
+++ task_07.py
@@ -0,0 +1,14 @@
+def combine_anagrams(words_array):
+    anagrams = {}
+    for word in words_array:
+        sorted_word = ''.join(sorted(word)).lower()
+        if sorted_word in anagrams:
+            anagrams[sorted_word].append(word.lower())
+        else:
+            anagrams[sorted_word] = [word.lower()]
+    print(anagrams.values())
+    return anagrams.values()
+
+
+combine_anagrams(["cars", "for", "potatoes", "racs", "four", "scar",
+"creams", "scream"]) # => [ ["cars", "racs", "scar"], ["four"], ["for"],["potatoes"], ["creams", "scream"] 
