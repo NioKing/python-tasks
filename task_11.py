bdcd71a9b93bf4b97ff6552d3c238550415c610c
Nio <nio@example.com>
2024-09-01 03:03:12 +0300

    11

---
+++ task_11.py
@@ -0,0 +1,34 @@
+class Dessert:
+    def __init__(self, name=None, calories=None):
+        self._name = name
+        self._calories = calories
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def calories(self):
+        return self._calories
+
+    @calories.setter
+    def calories(self, value):
+        self._calories = value
+
+    def is_healthy(self):
+        return self._calories < 200 if self._calories is not None else False
+
+    def is_delicious(self):
+        return True
+
+
+dessert1 = Dessert("Chocolate Cake", 300)
+
+print(dessert1.name)
+print(dessert1.calories)
+print(dessert1.is_delicious())
+print(dessert1.is_healthy())
