79ba08c31233c4e543b2e0d5e474b23595d24027
Nio <nio@example.com>
2024-09-03 04:32:59 +0300

    fix

--- task_05.py
+++ task_05.py
@@ -5,7 +5,12 @@ def date_in_future(days):
         new_date = datetime.now() + timedelta(days=days)
         return new_date.strftime('%d-%m-%Y %H:%M:%S')
     else:
-        return datetime.strftime('%d-%m-%Y %H:%M:%S')
+        return datetime.now().strftime('%d-%m-%Y %H:%M:%S')
 
+
+print(
 date_in_future([]) # => текущая дата
+)
+print(
 date_in_future(2) # => текущая дата + 2 дня
+)
